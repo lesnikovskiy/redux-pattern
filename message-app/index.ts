485cc931f0ecb2e6487a1f0bc75499eb23518028
Ruslan Lesnikovskiy <Ruslan.Lesnikovskiy@materialise.kiev.ua>
2019-03-21 18:08:38 +0200

    use DeleteMessageAction

--- message-app/index.ts
+++ message-app/index.ts
@@ -9,6 +9,7 @@ const unsubscribe = store.subscribe(() => console.log(store.getState()))
 
 store.dispatch({ type: "ADD_MESSAGE", message: "Hello World!!!" } as AddMessageAction);
 store.dispatch({ type: "ADD_MESSAGE", message: "Hello SUN!!!" } as AddMessageAction);
+store.dispatch({ type: "DELETE_MESSAGE", index: 1 } as DeleteMessageAction)
 store.dispatch({ type: "ADD_MESSAGE", message: "Hello Venus!!!" } as AddMessageAction);
 
 unsubscribe();
