4902375646018d46e694f699af2fc81b2fa10141
Ruslan Lesnikovskiy <Ruslan.Lesnikovskiy@materialise.kiev.ua>
2019-03-21 12:19:02 +0200

    Now add some actions

--- index.ts
+++ index.ts
@@ -2,7 +2,18 @@ import { Action } from "./Action";
 import { Reducer } from "./Reducer";
 
 let reducer: Reducer<number> = (state:  number, action: Action) => {
+    if (action.type === "INCREMENT") {
+        return state + 1;
+    }
+    if (action.type === "DECREMENT") {
+        return state - 1;
+    }
+
     return state;
 };
 
-console.log(reducer(0, null));
+const incrementAction: Action = { type: "INCREMENT" };
+console.log(reducer(1, incrementAction));
+
+const decrementAction: Action = { type: "DECREMENT" };
+console.log(reducer(1, decrementAction));
